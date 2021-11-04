0275106861069730bed1f4e1daea3c1cd93850a9
jesaelsoto <92120871+jesaelsoto@users.noreply.github.com>
2021-11-04 16:12:13 -0400

    welcome screen commit

---
+++ app/src/main/java/com/example/bp_fall_2021_quizapp/quizQuestionsActivity.java
@@ -0,0 +1,13 @@
+package com.example.bp_fall_2021_quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class quizQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_questions);
+    }
+}
