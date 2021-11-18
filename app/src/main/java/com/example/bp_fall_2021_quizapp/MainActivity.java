28edaf1075540a22f632eae43762003490d34787
jesaelsoto <92120871+jesaelsoto@users.noreply.github.com>
2021-11-18 15:40:41 -0500

    added jasmean's code

--- app/src/main/java/com/example/bp_fall_2021_quizapp/MainActivity.java
+++ app/src/main/java/com/example/bp_fall_2021_quizapp/MainActivity.java
@@ -9,10 +9,10 @@ import androidx.appcompat.widget.AppCompatEditText;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
-    public TextInputEditText username;
     // Put class variables up here
     // Best practice is to make them private (can only be accessed within the class, or using getters/setters)
     // Each UI component that you want to reference needs a variable
+     private TextInputEditText nameInput;
 
     /**
      * Method used to start an activity. It's the first method to run when the
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // obtain user's name using findViewById
-
+        nameInput = findViewById(R.id.username);
     }
 
     /**
@@ -34,11 +34,13 @@ public class MainActivity extends AppCompatActivity {
      */
     public void startQuiz(View view){
         // set name variable every time user clicks "start"
-
+        String name = nameInput.getText().toString();
         // If the name field is empty, prompt user to enter name
-
+        if (name.isEmpty())
+        {
+            Toast.makeText(getBaseContext(), "Please enter your name", Toast.LENGTH_SHORT).show();
+        }
         // If user has entered name, begin quiz
-
         Intent intent = new Intent(this, QuizQuestionActivity.class);
         startActivity(intent);
         finish(); // close current activity
